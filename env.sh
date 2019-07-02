d6d798979be97a9a971c5997ee25a46feca52827
Vinny Valdez <vvaldez@redhat.com>
2019-07-02 11:46:31 -0500

    Rename variables and support simpler query entry

--- env.sh
+++ env.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # SWSQL query to send to SolarWinds via REST API
-export NPM_PAYLOAD="query=SELECT CP.Asset_Group as Asset_Group, SysName, DNS, IP, NodeID, MachineType, CP.AppOwner, CP.Responsible_Teams, CP.Environment, CP.APP_Group, CP.APP_Group1, CP.APP_Group2, CP.APP_Group3, CP.Asset_Category, CP.Loc1_SiteCode, CP.Loc2_Region, CP.Loc3_Country FROM Orion.Nodes as N JOIN Orion.NodesCustomProperties as CP on N.NodeID = CP.NodeID WHERE CP.Asset_Group != '' AND N.SysName != '' AND N.DNS != '' AND N.MachineType != '' AND CP.AppOwner != '' AND CP.Responsible_Teams != '' AND CP.APP_Group != ''"
+export SW_QUERY="SELECT CP.Asset_Group as Asset_Group, SysName, DNS, IP, NodeID, MachineType, CP.AppOwner, CP.Responsible_Teams, CP.Environment, CP.APP_Group, CP.APP_Group1, CP.APP_Group2, CP.APP_Group3, CP.Asset_Category, CP.Loc1_SiteCode, CP.Loc2_Region, CP.Loc3_Country FROM Orion.Nodes as N JOIN Orion.NodesCustomProperties as CP on N.NodeID = CP.NodeID WHERE CP.Asset_Group != '' AND N.SysName != '' AND N.DNS != '' AND N.MachineType != '' AND CP.AppOwner != '' AND CP.Responsible_Teams != '' AND CP.APP_Group != ''"
 # Field in query results to use for hostname
 # e.g. "DNS"
-export NPM_HOSTNAME_FIELD="DNS"
+export SW_HOSTNAME_FIELD="DNS"
 # Field in query results that contains Operating System
 # e.g. "MachineType"
-export NPM_OS_FIELD="MachineType"
+export SW_OS_FIELD="MachineType"
 # comma-separated list of fields from query results to create and add hosts to as groups - these should NOT be NULL, ensure via payload query
-# e.g. NPM_GROUP_ON_FIELDS="Asset_Group,MachineType"
-export NPM_GROUP_ON_FIELDS=""
+# e.g. SW_GROUP_ON_FIELDS="Asset_Group,MachineType"
+export SW_GROUP_ON_FIELDS=""
 # comma-separated list of fields from query results to map to host variables - these should match columns defined in the payload query 
-# e.g. NPM_HOSTVAR_FIELDS="Asset_Group,SysName,DNS,IP,NodeID,MachineType,AppOwner,Responsible_Teams,Environment,APP_Group,APP_Group3,APP_Group2,APP_Group3,Asset_Category,Loc1_SiteCode,Loc2_Region,Loc3_Country"
-export NPM_HOSTVAR_FIELDS=""
+# e.g. SW_HOSTVAR_FIELDS="Asset_Group,SysName,DNS,IP,NodeID,MachineType,AppOwner,Responsible_Teams,Environment,APP_Group,APP_Group3,APP_Group2,APP_Group3,Asset_Category,Loc1_SiteCode,Loc2_Region,Loc3_Country"
+export SW_HOSTVAR_FIELDS=""
--- solarwinds.py
+++ solarwinds.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
 '''
-Custom dynamic inventory script for Ansible and Solar Winds, in Python.
+Custom dynamic inventory script for Ansible and SolarWinds, in Python.
 This was tested on:
-Orion v3
-ansible-2.7.10-1.el7ae.noarch
-ansible-tower-server-3.4.3-1.el7.x86_64
-Python 2.7.5 (default, Mar 26 2019, 22:13:06)
-[GCC 4.8.5 20150623 (Red Hat 4.8.5-36)] on linux2
+    Orion Platform 2018.2 HF3
+    ansible-2.7.10-1.el7ae.noarch
+    ansible-tower-server-3.4.3-1.el7.x86_64
+    Python 2.7.5 (default, Mar 26 2019, 22:13:06)
+    [GCC 4.8.5 20150623 (Red Hat 4.8.5-36)] on linux2
 
 (c) 2019 Vinny Valdez <vvaldez@redhat.com> and David Castellani <dcastell@redhat.com>
 
@@ -46,60 +46,66 @@ except ImportError:
 from urllib3 import disable_warnings, exceptions as urllib3exc
 disable_warnings(category=urllib3exc.InsecureRequestWarning)
 
-config_file = os.environ.get('NPM_INI_FILE') or 'solarwinds.ini'
+config_file = os.environ.get('SW_INI_FILE') or 'solarwinds.ini'
 
 #Get configuration variables
 config = ConfigParser.ConfigParser()
 config.readfp(open(config_file))
 
 # Orion Server IP or DNS/hostname
-server = os.environ.get('NPM_SERVER') or config.get('solarwinds', 'npm_server')
+server = os.environ.get('SW_SERVER') or config.get('solarwinds', 'sw_server')
 
 # Orion Username
-user = os.environ.get('NPM_USER') or config.get('solarwinds', 'npm_user')
+user = os.environ.get('SW_USER') or config.get('solarwinds', 'sw_user')
 
 # Orion Password
-password = os.environ.get('NPM_PASS') or config.get('solarwinds', 'npm_password')
+password = os.environ.get('SW_PASS') or config.get('solarwinds', 'sw_password')
 
-# Field for hostname
-hostname_field = os.environ.get('NPM_HOSTNAME_FIELD') or 'DNS'
+# Field to use for hostname
+hostname_field = os.environ.get('SW_HOSTNAME_FIELD') or 'DNS'
 
 # Field that contains Operating System
-os_field = os.environ.get('NPM_OS_FIELD') or 'MachineType'
+os_field = os.environ.get('SW_OS_FIELD') or 'MachineType'
 
 # List of fields to map to host variables 
-# these should match columns defined in the payload query. 
-# Specified as a comma-separated string, will be converted into an array
-hostvar_fields = os.environ.get('NPM_HOSTVAR_FIELDS') or "DNS,IP,Asset_Group"
-hostvar_fields = hostvar_fields.split(',')
+# These should match columns defined in the payload query. 
+# Specified as a comma-separated string, will be converted into a list
+# e.g. hostvar_fields = "DNS,IP,Asset_Group"
+hostvar_fields = os.environ.get('SW_HOSTVAR_FIELDS') or False
+if hostvar_fields:
+    hostvar_fields = hostvar_fields.split(',')
 
-# List of fields to to create and add hosts to
-group_on_fields = os.environ.get('NPM_GROUP_ON_FIELDS') or False
+# List of fields to to create and add hosts to as groups
+# IF empty string or False, no additional groups will be created other than OS groups
+# e.g. group_on_fields = "Asset_Group,MachineType"
+group_on_fields = os.environ.get('SW_GROUP_ON_FIELDS') or False
 if group_on_fields:
     group_on_fields = group_on_fields.split(',')
 
 # SWSQL query to send to SolarWinds via REST API
-payload = os.environ.get('NPM_PAYLOAD') or "query=SELECT CP.Asset_Group as Asset_Group, SysName, DNS, IP, MachineType FROM Orion.Nodes as N JOIN Orion.NodesCustomProperties as CP on N.NodeID = CP.NodeID"
+query = os.environ.get('SW_QUERY') or "SELECT CP.Asset_Group as Asset_Group, SysName, DNS, IP, MachineType FROM Orion.Nodes as N JOIN Orion.NodesCustomProperties as CP on N.NodeID = CP.NodeID"
 
 url = "https://"+server+":17778/SolarWinds/InformationService/v3/Json/Query"
 
 class SwInventory(object):
 
-    # CLI arguments
     def read_cli(self):
+        """ Parse CLI arguments and call proper method"""
         parser = argparse.ArgumentParser()
         parser.add_argument('--host')
         parser.add_argument('--list', action='store_true')
         self.options = parser.parse_args()
 
     def __init__(self):
+        """Run query against SolarWinds and build an Ansible-compatible JSON inventory"""
+
         # Initialize  inventory
         self.inventory = {'_meta': {'hostvars': {}}}
         self.read_cli_args()
 
         # Called with `--list`.
         if self.args.list:
-            self.query_results = self.get_hosts(payload)
+            self.query_results = self.get_hosts(query)
             self.inventory = self.write_hosts_to_inventory(self.inventory, self.query_results)
             self.inventory = self.create_os_groups(self.inventory)
             if group_on_fields:
@@ -119,20 +125,33 @@ class SwInventory(object):
         # Print inventory for Ansible to consume
         print(json.dumps(self.inventory, indent=2))
         
-    def get_hosts(self, payload):
-        req = requests.get(url, params=payload, verify=False, auth=(user, password))
+    def get_hosts(self, query):
+        """Execute SWSQL query against SolarWinds REST API and return results"""
+        
+        req = requests.get(url, params="query="+query, verify=False, auth=(user, password))
         query_results = req.json()
         return query_results
 
     def write_hosts_to_inventory(self, inventory, query_results):
-        # Add hosts and variables to inventory
+        """Iterate through results of query and create Ansible-compatible JSON dictionary with hosts 
+
+        The defined 'hostname_field' is used to assign the variable 'ansible_host' to the host inventory
+        If 'hostvar_fields' is defined, those fields will be mapped to host variables for each host
+        """
+
         for host in query_results['results']:
             inventory['_meta']['hostvars'].update({host[hostname_field]: {"ansible_host": host[hostname_field] }})
-            for field in hostvar_fields:
-                inventory['_meta']['hostvars'][host[hostname_field]][field] = max(host[field],'')
+            if hostvar_fields:
+                for field in hostvar_fields:
+                    inventory['_meta']['hostvars'][host[hostname_field]][field] = max(host[field],'')
         return inventory
 
     def add_hosts_to_group(self, inventory, query_results, group):
+        """Iterate through results of query and create groups for hosts that contain a matching field for the sent variable 'group'
+
+        e.g. all hosts with a field of 'MachineType' matching 'Red Hat' will be added to a group 'Red Hat'
+        """
+
         for host in query_results['results']:
             if host[group] in inventory:
                 if host[hostname_field] not in inventory[host[group]]['hosts']:
@@ -143,6 +162,11 @@ class SwInventory(object):
         return inventory
 
     def create_os_groups(self, inventory):
+        """Create groups to organize hosts into based on the variable 'os_field'
+
+        TODO: variablize these groups
+        """
+
         inventory.update({
             "all": {
                 "children": [
@@ -178,6 +202,10 @@ class SwInventory(object):
         return inventory
 
     def add_hosts_to_os_groups(self, inventory, query_results):
+        """Add hosts directly to OS groups matching their 'os_field'
+        NOTE: This is only called if no values were set for 'group_on_fields'
+        """
+
         for host in query_results['results']:
             if "Windows" in host[os_field]:
                 if host[hostname_field] not in inventory['Windows']['hosts']:
@@ -194,6 +222,8 @@ class SwInventory(object):
         return inventory
 
     def add_subgroups_to_os_groups(self, inventory, query_results):
+        """Add group that was created based on 'os_field' to matching OS group"""
+
         for host in query_results['results']:
             if "Windows" in host[os_field]:
                 if host[os_field] not in inventory['Windows']['children']:
